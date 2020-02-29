1147f87c966814f89b52a9c4cee558588845a9fd
abdullo-davlatov <abdullotjk99@gmail.com>
2020-02-29 15:27:16 -0500

    modified StringPractice class

--- src/day01/StringPratice.java
+++ src/day01/StringPratice.java
@@ -3,5 +3,6 @@ package day01;
 public class StringPratice {
     public static void main(String[] args) {
         System.out.println("Hello word");
+        System.out.println("Cybertek Batch 15");
     }
 }
